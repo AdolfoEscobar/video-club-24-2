1863e8855695e6fc4685bd7df5ba6ade6ab1c348
AdolfoEscobar <adolfoescobarpe@gmail.com>
2024-09-26 02:10:39 -0600

    Se inicia el db

---
+++ db.js
@@ -0,0 +1,19 @@
+const Sequelize =  require('sequelize');
+
+/*
+    1) Nombre de la base de datos
+    2) Usuario
+    3) Password
+    4) Objeto de configuracion <<ORM>>
+*/ 
+
+const sequelize = new Sequelize('video-club', 'root', 'abdc1234', {
+    host: '127.0.0.1',
+    dialect: 'mysql'
+});
+
+sequelize.sync({
+    force: true
+}).then(()=>{
+    console.log("Base de datos sincronizada correctamente.");
+});
