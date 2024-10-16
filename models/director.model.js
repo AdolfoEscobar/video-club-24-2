1c592015ec7ed9ce7d2afc2a6f5e35287d3f0878
AdolfoEscobar <adolfoescobarpe@gmail.com>
2024-10-16 00:46:56 -0600

    update class

--- models/director.model.js
+++ models/director.model.js
@@ -1,6 +1,10 @@
 module.exports = (sequelize, type) => {
     const Director = sequelize.define('directors', {
-        id: {type: type.INTEGER, primaryKey: true, autoIncrement: true},
+        id: {
+            type: type.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
         name: type.STRING,
         lastName: type.STRING
     });
